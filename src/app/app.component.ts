a6824ada8554d7ac554e0c16d1e8208530839ddc
Joshua-McCann <jmcsquiggle@gmail.com>
2018-08-19 21:05:53 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Capstone';
+export class AppComponent implements OnInit {
+  title = 'Home';
+  logout = true;
+
+  public ngOnInit() {
+    console.log('App init');
+  }
+
+  public goTo(location){
+    this.title = location;
+  }
+
+  public loginPress() {
+    this.logout = !this.logout;
+    if (this.logout) {
+      this.title = "logout";
+    } else {
+      this.title = "login";
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TopicComponent } from './topic/topic.component';
+import { PostsComponent } from './posts/posts.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'topic', component: TopicComponent },
+  { path: 'topic?:id', component: PostsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full'}
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopicComponent,
+    PostsComponent,
+    PageNotFoundComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
-    BrowserModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
